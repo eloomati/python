9833bedaf0526cf82b547cd0e4be6bcc5231d8a8
eloomati <44767611+eloomati@users.noreply.github.com>
2021-03-08 19:21:20 +0100

    Add files via upload

---
+++ Zad3.py
@@ -0,0 +1,7 @@
+import funkcjeZad1.Zad3
+
+a=int(input("Jaki jest twój kapitał początkowy? "))
+b=int(input("Jakie jest oprocentowanie? "))
+c=int(input("Przez jaki czas chcesz inwestowa"))
+
+funkcjeZad1.Zad3.lokata(a,b,c)
