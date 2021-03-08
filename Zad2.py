2c650dbd78928d51741a189f5e2966e025e565bc
eloomati <44767611+eloomati@users.noreply.github.com>
2021-03-08 19:04:57 +0100

    Pierwsze zadania

---
+++ Zad2.py
@@ -0,0 +1,6 @@
+import funkcjeZad1.Zad2
+
+a=int(input("Wartość jednej przewprostokątnej to?: "))
+b=int(input("Wartość drugiej przeciwprostokątnej to?: "))
+
+funkcjeZad1.Zad2.przeciw(a,b)
---
+++ helloword.py
@@ -0,0 +1,6 @@
+import funkcjeZad1.Zad1
+
+a=int(input("Jaki pokonałeś dystans? "))
+b=int(input("Jaki miałes czas? "))
+
+funkcjeZad1.Zad1.liczenie_sredniej(a,b)
